9b64a0dc1670aa2b94e643be51ff6fc3a10d9599
Henry Faulkner <hlf46553@uga.edu>
2022-03-08 00:23:20 -0500

    created liquor shooter vending machine with the state pattern

---
+++ StatePattern/SimpleShooterVendingMachine/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SimpleShooterVendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VendingMachine vm = new VendingMachine(10);
+
+            while (vm.GetState() != vm.GetSOLD_OUT())
+            {
+                Console.Write("1 to insert dollar.\n2 to press button.\n3 to eject your dollar.\n");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        vm.InsertDollar();
+                        break;
+                    case "2":
+                        vm.PressButton();
+                        break;
+                    case "3":
+                        vm.EjectDollar();
+                        break;
+                }
+            }
+        }
+    }
+}
